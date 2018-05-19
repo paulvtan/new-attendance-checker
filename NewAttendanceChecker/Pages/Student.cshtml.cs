812a9e61359c7d28b8c6410a657f3adb432a393f
Khoa Nguyen <khoanguyen0497@gmail.com>
2018-05-20 10:48:37 +1200

    done

--- NewAttendanceChecker/Pages/Student.cshtml.cs
+++ NewAttendanceChecker/Pages/Student.cshtml.cs
@@ -42,25 +42,22 @@ namespace NewAttendanceChecker.Pages
         }
         public async Task<IActionResult> OnPostAsync(string checkInCode, string UPI, string realCode)
         {
-            Attendance Attendace;
-            Student = _context.Students
-                .Include(s => s.AttendanceList)
-                .Include(s => s.CourseTags)
-                    .ThenInclude(s => s.Course)
-                        .ThenInclude(s => s.Lecturer)
-                .FirstOrDefault(s => s.StudentID == UPI);
-
-            AttendanceList = _context.AttendanceList
-                .Include(a => a.Session)
-                    .ThenInclude(s => s.Course)
-                .Where(a => a.StudentID == UPI)
-                .ToList();
-            Sessions = _context.Sessions
-                .Where(s => Student.CourseTags.Select(c => c.CourseID).Contains(s.CourseID)) //only sessions of my courses
-                .Where(s => !AttendanceList.Select(a => a.SessionID).Contains(s.SessionID))
-                .ToList();
+            Attendance Attendance;
+            
             if (checkInCode != null)
             {
+                if (checkInCode.ToLower() == realCode.ToLower())
+                {
+                    Attendance = new Attendance
+                    {
+                        StudentID = UPI,
+                        SessionID = realCode
+                    };
+                    await _context.AttendanceList.AddAsync(Attendance);
+                    await _context.SaveChangesAsync();
+                }
+                
+
             }
             return RedirectToPage();
         }
