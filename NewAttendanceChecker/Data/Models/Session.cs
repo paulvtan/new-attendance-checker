1f0e298747c43964f919599f0d919a963cbb1e8d
Khoa Nguyen <khoanguyen0497@gmail.com>
2018-05-19 16:23:43 +1200

    database done

---
+++ NewAttendanceChecker/Data/Models/Session.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewAttendanceChecker.Data.Models
+{
+    public class Session
+    {
+        [Key]
+        public string SessionID { get; set; }
+        public string CourseID { get; set; }
+
+        [ForeignKey("CourseID")]
+        public Course Course { get; set; }
+
+
+    }
+}
